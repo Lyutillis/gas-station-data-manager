690d61e02c22f67cea1aa9febd546b25442b6551
Lytillis <eugene.chemuranov@data-ox.com>
2024-05-20 18:13:24 +0300

    Fixed migrations

--- config/settings.py
+++ config/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+SECRET_KEY = "uvt)27=0cd$egsk35q9blzl1&2u3_8h4vzyvlw1xs2@9ki6z5u"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
